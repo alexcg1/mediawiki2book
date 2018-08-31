be17029b7c2280dd05936c311b6fa42b6247adb3
Alex C-G <alexcg@outlook.com>
2018-08-31 16:22:21 +0800

    Verbosity and debug options. Better language handling

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # mediawiki2book
 
-Small Python script to take a wikitext file and turn it into a beautiful PDF. This is primarily aimed at my colleagues who regularly convert material from toyhouse.cc, namely the XLP Owner's Manual
+Small Python script to take a MediaWiki file and turn it into a beautiful PDF. This is aimed at my colleagues who regularly convert material from http://toyhouse.cc, namely the XLP Owner's Manual, but I'm happy for anyone to use or reuse this code.
 
 ## This repo contains:
 
 * media2wiki.py - the script to export from wiki to pretty pdf book format
 * xlp_manual_dev.mediawiki - an example mediawiki file for you to process
 * xlp_manual_dev_example.pdf - exported example file from the mediawiki file above
-* images.zip - the images referenced in the mediawiki file
+* images/ - the images referenced in the example mediawiki file
 * resources/header.yaml - metadata for the exported PDF
-* resources/firevogel.latex - latex template to render the PDF
+* resources/templates/firevogel.latex - latex template to render the PDF
 
 ## You will need:
 
@@ -17,15 +17,27 @@ Small Python script to take a wikitext file and turn it into a beautiful PDF. Th
 * Pandoc (preferably 2.0 or higher)
 * A lot of LaTeX packages. I didn't keep track, but they're all available by default in Ubuntu 18.04
 
-## Instructions
+## Usage
 
-1. Copy resources/firevogel.latex to $HOME/.pandoc/templates
-2. Edit mediawiki2book.py and set the image directory as needed. Edit resources/header.yaml to change paper size, title page color, etc
-3. Copy your mediawiki file to mediawiki2book directory. Make sure the name is *your_filename.mediawiki*
-4. Run ./mediawiki2book *your_filename.mediawiki* and wait a while. It will eventually spit out *yourfilename.pdf*.
+1. Copy your mediawiki page source to a file in your working directory (called *something.mediawiki*, and put any linked images into 'images' folder in the same directory
+2. Edit resources/header.yaml to change paper size, title page color, etc
+3. Run ./mediawiki2book *your_filename.mediawiki* and wait a while. It will eventually spit out *yourfilename.pdf*.
+
+mediawiki2book works pretty well with English (by default) and Chinese (*--lang zh*). No guarantees about it working for other languages yet, but feel free to push changes to make that happen!
 
 ## Troubleshooting
 
 ### I don't have the latest Pandoc!
 
 Ubuntu comes with an old version of Pandoc. Download the latest from [Pandoc's website](https://pandoc.org/installing.html)
+
+### Chinese/other CJK language is coming out wrong
+
+* Run mediawiki2book with *--lang zh* or whatever your language is. Right now it's confirmed to work in Chinese and English. To add new languages you should see the code about "mainfont" in the YAML header
+* Make sure you have Chinese fonts installed
+* Check the [Pandoc wiki on Chinese stuff](https://github.com/jgm/pandoc/wiki/Pandoc-with-Chinese) for other troubleshooting
+
+### Something else is going wrong
+
+* Run mediawiki2book with the *--debug* argument to check your variables (like filenames, etc) are okay
+* Run mediawiki2book with the *--veryverbose* argument to see all of pandoc's debugging info
--- mediawiki2book.py
+++ mediawiki2book.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
 import argparse
-import subprocess
 from subprocess import call
 import os
 import shutil
-import time
+from datetime import date, datetime, time
+from babel.dates import format_date
 import tempfile
 import sys
 from pathlib import Path
+
+# I'd like to have automatic language detection. However when it reads a mediawiki file it just says it's "en"
+# try:
+# 	from langdetect import detect
+# 	print("* Language detection enabled")
+# except:
+# 	print("* To detect file language, please install langdetect python module")
 
 #### These are the main things you may want to edit! ####
 
@@ -19,8 +26,6 @@ images_dir = buildsystem_dir+"/images/"
 #########################################################
 
 class bcolors:
-	HEADER = '\033[95m'
-	OKBLUE = '\033[94m'
 	OKGREEN = '\033[92m'
 	WARNING = '\033[93m'
 	FAIL = '\033[91m'
@@ -37,10 +42,23 @@ fail_text = bcolors.FAIL + "[ FAIL ]" + bcolors.ENDC
 parser = argparse.ArgumentParser(description="Convert MediaWiki files to other formats")
 parser.add_argument("filename", help="The MediaWiki file you want to convert")
 parser.add_argument("--debug", action='store_true', default=False, help="Keep temporary files for debugging purposes and give verbose output")
+parser.add_argument("--veryverbose", action='store_true', default=False, help="Turn on Pandoc's verbose output")
 parser.add_argument("-t", "--to", default="pdf", help="Format you want to export to. Accepts any format accepted by pandoc. Default is PDF") # Untested!
-parser.add_argument("--cjk", action='store_true', default=False) # This is a kluge. How can we programmatically figure out language?
-parser.add_argument("--type", default="book", choices=["article", "book"])
+parser.add_argument("--type", default="book", choices=["article", "book"], help="What type of document you want. Books are separated and numbered by chapter, and have larger inner margins")
+parser.add_argument("--lang", default="en", help="Set language, e.g. zh or en")
 args = parser.parse_args()
+
+def cjkcheck():
+	global header_date
+	if args.lang == "zh":
+		# header_cjkfont = "Noto Sans CJK SC Regular"
+		header_date = format_date(datetime.today(), locale='zh_CN')
+	# elif args.lang == "kr":
+	# 	header_cjkfont = "Noto Sans CJK KR Regular"
+	# elif args.lang == "jp":
+	# 	header_cjkfont = "Noto Sans CJK JP Regular"
+	else:
+		header_date = format_date(datetime.today(), locale='en_US')
 
 ## Define important variables
 output_format = args.to
@@ -67,13 +85,20 @@ print(ok_text)
 ########## Deal with the raw mediawiki file
 
 # Remove references section - they're all converted to footnotes anyway, so we don't need the title for an empty section
-print("* Removing wiki-only stuff........................................", end="", flush=True)
 
 # Safely read the input filename using 'with'
 with open(input_file) as f:
 	s = f.read()
+	# Get input file language
+	# try:
+	# 	lang = detect(s)
+	# 	print("* Detecting language..............................................", end="", flush=True)
+	# 	print("[ ",lang, " ]")
+	# except:
+	# 	pass
 
 with open(input_file, 'w') as f:
+	print("* Removing wiki-only stuff........................................", end="", flush=True)
 	s = s.replace("= Before Reading =", "")
 	s = s.replace("= References =", "")
 	f.write(s)
@@ -94,30 +119,18 @@ print(ok_text)
 
 ### Add YAML header
 
-if args.cjk:
-	header = resources_dir+"/header-cjk.yaml" # This is a kluge. Should really add these lines into the header.yaml programatically
-else:
-	header = resources_dir+"/header.yaml"
+cjkcheck() # Check if CJK or not
 
-if Path(header).is_file():
+header_file = resources_dir+"/header.yaml"
+
+if Path(header_file).is_file():
 	print("* Adding metadata header..........................................", end="", flush=True)
-	header=open(header, "r").read() # header file to string
+	header = open(header_file, "r").read() # header file to string
+	header = header.replace("$DATE", header_date)
 
-	# Change date to today
-	day = time.strftime("%d")
-	year = time.strftime("%Y")
-
-	if args.cjk: # Can we replace this with a library that will write date in proper format??
-		month = time.strftime("%m")
-	else:
-		month = time.strftime("%B")
-	
-	if args.cjk:
-		todaydate = year+"年"+month+"月"+day+"日"
-	else:
-		todaydate = day+" "+month+", "+year
-	
-	header = header.replace("$DATE", todaydate)
+	if args.lang == "zh":
+		header = header.split("---")[1]
+		header = "---"+header+"mainfont: Noto Sans Mono CJK SC Regular\n---"
 
 	# Prepend to markdown file
 	with open(md_file, 'r') as original: data = original.read()
@@ -130,30 +143,50 @@ else:
 	print(warning_text)
 	warnings.append("No header.yaml\nWe couldn't find header.yaml in your resources directory. This means that a nice cover and title/author info may be missing from your PDF")
 
-# Convert to output (default pdf) using modified eisvogel template (https://github.com/Wandmalfarbe/pandoc-latex-template)
+# Convert to output (default pdf)
 print("* Converting to", output_format, "(this may take a while).......................", end="", flush=True)
 
-# pandoc_args = [md_file, "-o", buildsystem_dir+"/"+output_file, "--toc", "--top-level-division=chapter", "--template="+template, "--listings", "--pdf-engine=xelatex", "--toc-depth=3", "--data-dir="+resources_dir]
-pandoc_args = [md_file, "-o", buildsystem_dir+"/"+output_file, "--toc", "--template="+template, "--listings", "--pdf-engine=xelatex", "--toc-depth=3", "--data-dir="+resources_dir]
-
-if args.cjk:
-	pandoc_args.append("-V lang=zh")
-
-if args.debug:
-	pandoc_args.append("--verbose")
+pandoc_args = [
+	md_file,
+	"-o",						# Sets output filename
+	buildsystem_dir+"/"+output_file,
+	"--toc", 					# Table of contents
+	"--template="+template,		# Firevogel by default for books
+	"--listings", 				# Make code blocks stay in page and look nice
+	"--pdf-engine=xelatex", 	# Works better with CJK
+	"--toc-depth=3", 			# Depth of page of contents
+	"--data-dir="+resources_dir,# Looks for templates in ./resources
+	"-V lang="+args.lang
+	]
 
 if args.type == "book":
 	pandoc_args.append("--top-level-division=chapter")
 
+if args.veryverbose:
+	pandoc_args.append("--verbose")
+
 pandoc_command = [pandoc]
 pandoc_command.extend(pandoc_args) # Add arguments to the command
+
+if args.debug:
+	print("\n")
+	print("-- DEBUG INFORMATION ------------------------------------------\n")
+	print("* Working directory: 	",buildsystem_dir)
+	print("* Input file: 		",input_file)
+	print("* Intermediate file: 	",md_file)
+	print("* YAML header: 		",header_file)
+	print("* Output file: 		",output_file)
+	print("* Language: 			",args.lang)
+	print("* Book or article: 	",args.type)
+	print("* Pandoc command:  	 pandoc",' '.join(pandoc_args))
+	print("\n---------------------------------------------------------------")
 
 try:
 	call(pandoc_command)
 	print(ok_text)
 
 	# Clean up temp files (unless debug argument is passed)
-	if args.debug == 0:
+	if args.debug == False:
 		print("* Cleaning up temp directory......................................", end="", flush=True)
 		try:
 			shutil.rmtree(temp_dir)
@@ -165,16 +198,15 @@ try:
 		print("* Leaving temp files at", temp_dir, "..................................", end="", flush=True)
 		print(ok_text)
 
-	
 	print("\nYour", output_format, "file has been created at", output_file, ". ", end="", flush=True)
+
+	reply = str(input("View "+output_file+" now? (y/n): ")).lower().strip()
+	if reply[0] == 'y':
+		os.system('/usr/bin/xdg-open'+' '+buildsystem_dir+'/'+output_file)
+	elif reply[0] == 'n':
+		exit()
 
 except:
 	print(fail_text)
 	print("\nSomething went wrong. We're keeping your temp files at", temp_dir)
-	exit()
-
-reply = str(input('View output file now? (y/n): ')).lower().strip()
-if reply[0] == 'y':
-	os.system('/usr/bin/xdg-open'+' '+buildsystem_dir+'/'+output_file)
-elif reply[0] == 'n':
 	exit()
