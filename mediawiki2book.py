5a97ff586984f208004f39928eb317d64ec5ee58
Alex C-G <alexcg@outlook.com>
2018-09-05 16:20:18 +0800

    Better YAML handling

--- mediawiki2book.py
+++ mediawiki2book.py
@@ -9,19 +9,13 @@ from babel.dates import format_date
 import tempfile
 import sys
 from pathlib import Path
-
-# I'd like to have automatic language detection. However when it reads a mediawiki file it just says it's "en"
-# try:
-# 	from langdetect import detect
-# 	print("* Language detection enabled")
-# except:
-# 	print("* To detect file language, please install langdetect python module")
+import yaml
 
 #### These are the main things you may want to edit! ####
 
-buildsystem_dir = os.getcwd()
-resources_dir = buildsystem_dir+"/resources"
-images_dir = buildsystem_dir+"/images/"
+working_dir = os.getcwd() # What directory are we in now?
+resources_dir = working_dir+"/resources"
+images_dir = working_dir+"/images/"
 
 #########################################################
 
@@ -43,30 +37,24 @@ parser = argparse.ArgumentParser(description="Convert MediaWiki files to other f
 parser.add_argument("filename", help="The MediaWiki file you want to convert")
 parser.add_argument("--debug", action='store_true', default=False, help="Keep temporary files for debugging purposes and give verbose output")
 parser.add_argument("--veryverbose", action='store_true', default=False, help="Turn on Pandoc's verbose output")
+parser.add_argument("-f", "--fromformat", default="mediawiki", help="Format you want to export from. Accepts any format accepted by pandoc. Default is MediaWiki") # Untested!
 parser.add_argument("-t", "--to", default="pdf", help="Format you want to export to. Accepts any format accepted by pandoc. Default is PDF") # Untested!
 parser.add_argument("--type", default="book", choices=["article", "book"], help="What type of document you want. Books are separated and numbered by chapter, and have larger inner margins")
 parser.add_argument("--lang", default="en", help="Set language, e.g. zh or en")
 args = parser.parse_args()
 
-def cjkcheck():
-	global header_date
-	if args.lang == "zh":
-		# header_cjkfont = "Noto Sans CJK SC Regular"
-		header_date = format_date(datetime.today(), locale='zh_CN')
-	# elif args.lang == "kr":
-	# 	header_cjkfont = "Noto Sans CJK KR Regular"
-	# elif args.lang == "jp":
-	# 	header_cjkfont = "Noto Sans CJK JP Regular"
-	else:
-		header_date = format_date(datetime.today(), locale='en_US')
-
 ## Define important variables
+input_file = args.filename
+input_format = args.fromformat
 output_format = args.to
 pandoc = "/usr/bin/pandoc"
 
-input_file = args.filename
-md_file = input_file.replace("mediawiki", "md")
-output_file = input_file.replace("mediawiki", output_format)
+if input_format == "md":
+	md_file = input_file
+else:
+	md_file = input_file.replace(input_format, "md") # Markdown is universal intermediate format
+	
+output_file = input_file.replace(input_format, output_format)
 
 if args.type == "article":
 	template = "eisvogel" # This is the base eisvogel template
@@ -87,68 +75,42 @@ except:
 	print("Creating temp directory anyway")
 	os.mkdir(temp_dir)
 
+############# Now we can convert to Markdown
 
+if input_format != "md": # If it's already Markdown we dont need to convert
+	print("* Converting to MarkDown for pre-processing.......................", end="", flush=True)
+	call(["pandoc", "--from", args.fromformat, input_file, "-t", "markdown", "-o", temp_dir+"/"+md_file])
+	print(ok_text)
 
-########## Deal with the raw mediawiki file
-
-# Remove references section - they're all converted to footnotes anyway, so we don't need the title for an empty section
-
-# # Safely read the input filename using 'with'
-# with open(input_file) as f:
-# 	s = f.read()
-# 	# Get input file language
-# 	# try:
-# 	# 	lang = detect(s)
-# 	# 	print("* Detecting language..............................................", end="", flush=True)
-# 	# 	print("[ ",lang, " ]")
-# 	# except:
-# 	# 	pass
-# 
-# with open(input_file, 'w') as f:
-# 	print("* Removing wiki-only stuff........................................", end="", flush=True)
-# 	s = s.replace("= Before Reading =", "")
-# 	s = s.replace("= References =", "")
-# 	f.write(s)
-# 
-# f.close()
-# 
-# print(ok_text)
-# 
-# ############# Now we can convert to Markdown
-
-print("* Converting to MarkDown for pre-processing.......................", end="", flush=True)
-call(["pandoc", "--from=mediawiki", input_file, "-t", "markdown", "-o", temp_dir+"/"+md_file])
-print(ok_text)
-
-print("* Changing to temp directory......................................", end="", flush=True)
-os.chdir(temp_dir)
-print(ok_text)
+	print("* Changing to temp directory......................................", end="", flush=True)
+	os.chdir(temp_dir)
+	print(ok_text)
 
 ### Add YAML header
 
-cjkcheck() # Check if CJK or not
+print("* Processing header file................................................."", end="", flush=True)
 
 header_file = resources_dir+"/header.yaml"
 
 if Path(header_file).is_file():
-	print("* Adding metadata header..........................................", end="", flush=True)
-	header = open(header_file, "r").read() # header file to string
-	header = header.replace("$DATE", header_date)
+	header_string = open(header_file, "r").read()
+	header = yaml.load(header_string)
+
+	header["date"] = format_date(datetime.today(), locale=args.lang) # Insert today's date and format by country
 
 	if args.lang == "zh":
-		header = header.split("---")[1]
-		header = "---"+header+"mainfont: Noto Sans Mono CJK SC Regular\n---"
+		header["mainfont"] = 'Noto Sans CJK SC Regular'
 
-	# Prepend to markdown file
+	header_text = "---\n"+yaml.dump(header)+"---"
+
 	with open(md_file, 'r') as original: data = original.read()
-	with open(md_file, 'w') as modified: modified.write(header + "\n" + data)
+	with open(md_file, 'w') as modified: modified.write(header_text + "\n" + data)
 
 	print(ok_text)
 
 else:
 	print("* No metadata header found....................................", end="", flush=True)
 	print(warning_text)
-	warnings.append("No header.yaml\nWe couldn't find header.yaml in your resources directory. This means that a nice cover and title/author info may be missing from your PDF")
 
 # Convert to output (default pdf)
 print("* Converting to", output_format, "(this may take a while).......................", end="", flush=True)
@@ -156,7 +118,7 @@ print("* Converting to", output_format, "(this may take a while)................
 pandoc_args = [
 	md_file,
 	"-o",						# Sets output filename
-	buildsystem_dir+"/"+output_file,
+	working_dir+"/"+output_file,
 	"--toc", 					# Table of contents
 	"--template="+template,		# Firevogel by default for books
 	"--listings", 				# Make code blocks stay in page and look nice
@@ -178,7 +140,8 @@ pandoc_command.extend(pandoc_args) # Add arguments to the command
 if args.debug:
 	print("\n")
 	print("-- DEBUG INFORMATION ------------------------------------------\n")
-	print("* Working directory: 	",buildsystem_dir)
+	print("* Working directory: 	",working_dir)
+	print("* Resources directory:   ",resources_dir)
 	print("* Input file: 		",input_file)
 	print("* Intermediate file: 	",md_file)
 	print("* YAML header: 		",header_file)
@@ -209,11 +172,11 @@ try:
 
 	reply = str(input("View "+output_file+" now? (y/n): ")).lower().strip()
 	if reply[0] == 'y':
-		os.system('/usr/bin/xdg-open'+' '+buildsystem_dir+'/'+output_file)
+		os.system('/usr/bin/xdg-open'+' '+working_dir+'/'+output_file)
 	elif reply[0] == 'n':
 		exit()
 
 except:
 	print(fail_text)
-	print("\nSomething went wrong. We're keeping your temp files at", temp_dir)
+	print("\nSomething went wrong with pandoc. We're keeping your temp files at", temp_dir)
 	exit()
